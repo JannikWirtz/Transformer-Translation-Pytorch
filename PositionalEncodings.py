50a3a14ab38492dbaa31d220f8b81ad9dfecde3f
JannikWirtz <jannik.wirtz@yahoo.de>
2023-10-24 11:27:48 +0200

    typo fix

--- PositionalEncodings.py
+++ PositionalEncodings.py
@@ -11,14 +11,12 @@ class PositionalEncodings(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
 
         pe = torch.zeros(self.seq_len, self.model_d) # (seq_len, d_model)
-
         pos = torch.arange(0, self.seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
-
         # use log for numerical stability
         denominator = torch.exp(torch.arange(0, self.model_d, 2).float() * (-math.log(10000.0) / self.model_d))
 
-        pos[:, 0::2] = torch.sin(pos * denominator) # all even columns (sin)
-        pos[:, 1::2] = torch.cos(pos * denominator) # all odd columns (cos)
+        pe[:, 0::2] = torch.sin(pos * denominator) # all even columns (sin)
+        pe[:, 1::2] = torch.cos(pos * denominator) # all odd columns (cos)
 
         pe = pe.unsequenze(0) # (1, seq_len, d_model)
         self.register_buffer('pe', self.pe) # add pe to the model but not to the parameters
