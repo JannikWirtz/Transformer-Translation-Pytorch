ec91e58a5aab16e8b64a44f608904475faa7aed6
JannikWirtz <jannik.wirtz@yahoo.de>
2023-10-24 12:46:29 +0200

    Add Residual/Skip connection

---
+++ ResidualConnection.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import LayerNorm
+
+
+class ResidualConnection(nn.Module):
+    
+    def __init__(self, dropout_rate) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.norm = LayerNorm()
+
+    def forward(self, x: torch.Tensor, sublayer: nn.Module) -> torch.Tensor:
+        # using the more modern pre-norm here; instead of original post-norm
+        return x + self.dropout(sublayer(self.norm(x)))
