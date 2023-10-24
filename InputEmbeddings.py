e0b98de8323cc7992856904a0283dc4bde5e7eff
JannikWirtz <jannik.wirtz@yahoo.de>
2023-10-24 11:11:26 +0200

    add Input Embedding

---
+++ InputEmbeddings.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(self.vocab_size, self.d_model)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.embedding(x) * math.sqrt(self.d_model)
